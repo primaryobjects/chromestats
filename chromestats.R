8e54b49750323ef4fc0efbfb5fedf20fa47312eb
K <kbecker@primaryobjects.com>
2016-01-01 11:51:07 -0500

    Changed driver from RMongo to mongolite for Mongodb 3.0 support.

--- chromestats.R
+++ chromestats.R
@@ -1,26 +1,20 @@
 # Install packages if needed.
-packages <- c("RMongo", "ggplot2")
+packages <- c("mongolite", "ggplot2")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
 
-require(RMongo)
+library(mongolite)
 require(ggplot2)
 
-# Connect to QA database.
-mongo <- mongoDbConnect("dbname", "host.mongo.com", 12345)
+# Connect to database.
+mongo <- mongo('collection_name', url = 'mongodb://user:pw@host.mongo.com:12345/dbname')
 
-# Login.
-auth <- dbAuthenticate(mongo, "username", "password")
-
-# Find all published stories.
-docs <- dbGetQuery(mongo, "analytics", "", 0, 9999999)
-
-# Disconnect from the database.
-dbDisconnect(mongo)
+# Find all records.
+docs <- mongo$find()
 
 # Format date column. Remove EST or EDT from dates.
-docs$date <- as.POSIXlt(gsub("EDT|EST", "", docs$EventDate), format = "%a %b %d %H:%M:%S %Y")
+docs$date <- as.POSIXlt(gsub("EDT|EST", "", docs$EventDate), format = "%Y-%m-%d %H:%M:%S")
 
 # Order data by date.
 sorted <- docs[order(docs$date),]
@@ -28,3 +22,4 @@ sorted <- docs[order(docs$date),]
 # Plot chart.
 g <- ggplot(data=sorted, aes(x=date, y=Users)) + xlab("Date") + ggtitle("My Extension Active Users") + geom_line()
 g <- g + geom_smooth()
+g
